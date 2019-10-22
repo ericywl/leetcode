d2ea9c86e924548f77363e7e7b445c4f97729528
ericywl <ericywl@live.com>
2019-10-22 19:47:27 +0800

    Add longest palindromic substring

---
+++ python/longest_palindrome_substr.py
@@ -0,0 +1,25 @@
+class Solution:
+    def addBorders(self, s: str) -> str:
+        return "#" + "#".join([c for c in s]) + "#"
+        
+    def removeBorders(self, s: str) -> str:
+        return "".join(s.split("#"))
+
+    def longestPalindrome(self, s: str) -> str:
+        if not s:
+            return ""
+
+        pali = s[0]
+        s = self.addBorders(s)
+        for i in range(1, len(s) - 1):
+            for l in range(i - 1, -1, -1):
+                r = 2 * i - l
+                if r >= len(s) or s[l] != s[r]:
+                    break
+                pali = s[l:r + 1] if len(s[l:r + 1]) > len(pali) else pali
+        return self.removeBorders(pali)
+
+
+if __name__ == "__main__":
+    sol = Solution()
+    print(sol.longestPalindrome("adfnwejrdabbad"))
